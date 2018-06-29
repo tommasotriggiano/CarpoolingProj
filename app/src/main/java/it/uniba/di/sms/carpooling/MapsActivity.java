b56673e809481f096936b1a36ad5f94eb1518622
tommaso <tommaso.triggiano@gmail.com>
2018-06-29 13:42:28 +0200

    MapsActivity + dependencies firestore 11.4.2

--- app/src/main/java/it/uniba/di/sms/carpooling/MapsActivity.java
+++ app/src/main/java/it/uniba/di/sms/carpooling/MapsActivity.java
@@ -1,7 +1,10 @@
 package it.uniba.di.sms.carpooling;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,15 +12,55 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private String tipoViaggio,data,ora,nome;
+    CollectionReference passaggi;
+    DocumentReference user;
+    FirebaseUser userAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        Intent receive = getIntent();
+        tipoViaggio = receive.getStringExtra("tipoViaggio");
+        data = receive.getStringExtra("data");
+        ora = receive.getStringExtra("ora");
+        nome = receive.getStringExtra("nome");
+
+        userAuth = FirebaseAuth.getInstance().getCurrentUser();
+        passaggi = FirebaseFirestore.getInstance().collection("Rides");
+        user = FirebaseFirestore.getInstance().collection("Users").document(userAuth.getUid());
+
+
+
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -37,10 +80,93 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        readData(new FirestoreCallback() {
+            @Override
+            public void onCallback(Map<String, Object> userCompany) {
+                Query findRides;
+                String userNameCompany = (String) userCompany.get("name");
+                final LatLng lavoro = new LatLng((Double)userCompany.get("latitude"),(Double)userCompany.get("longitude"));
+                final SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm");
+                Date date= null;
+                try{date = dateFormat.parse(ora);
+                }
+                catch(ParseException p){
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                }
+                final Date oraMin= date;
+                oraMin.setMinutes(date.getMinutes()-10);
+                final Date oraMax= date;
+                oraMax.setMinutes(date.getMinutes()+10);
+
+                if((nome.isEmpty())){
+                    findRides = passaggi.whereEqualTo("dataPassaggio",data).whereEqualTo("tipoViaggio",tipoViaggio).whereEqualTo("autista.userCompany.name",userNameCompany);
+                }
+                else{
+                    findRides = passaggi.whereEqualTo("dataPassaggio",data).whereEqualTo("tipoViaggio",tipoViaggio).whereEqualTo("autista.surname",nome).whereEqualTo("autista.userCompany.name",userNameCompany);}
+
+                Toast.makeText(MapsActivity.this,userNameCompany,Toast.LENGTH_LONG).show();
+                findRides.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        mMap.addMarker(new MarkerOptions().position(lavoro).title("Marker in ..."));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(lavoro));
+                        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+
+                        for(DocumentSnapshot document : task.getResult()){
+                            Map<String,Object> passaggio = document.getData();
+                            Map<String,Object> autista = (Map<String,Object>) passaggio.get("autista");
+                            Map<String,Object> address = (Map<String,Object>) autista.get("userAddress");
+                            Double latitude = (Double) address.get("latitude");
+                            Double longitude = (Double) address.get("longitude");
+                            String oraPartenza =(String) passaggio.get("ora");
+                            Date oraP = null;
+                            try {
+                                oraP = dateFormat.parse(oraPartenza);
+                                Toast.makeText(MapsActivity.this,oraP.toString(),Toast.LENGTH_LONG).show();;
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+
+                            if(!(userAuth.getUid().equals(autista.get("id")))&&(oraP.after(oraMin))&&(oraP.before(oraMax))){
+
+                                LatLng indirizzo = new LatLng(latitude,longitude);
+
+                                mMap.addMarker(new MarkerOptions().position(indirizzo).title("Marker in ..."));
+                                mMap.moveCamera(CameraUpdateFactory.newLatLng(indirizzo));}
+
+
+
+
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+        }
+
+        private void readData(final FirestoreCallback callback){
+
+            user.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    Map<String,Object> user = documentSnapshot.getData();
+                    Map<String,Object> userCompanyAddress = (Map<String,Object>)user.get("userCompany");
+                    callback.onCallback(userCompanyAddress);
+
+                }
+            });
     }
-}
+
+    //interfaccia per metodo di callback
+    private interface FirestoreCallback {
+        void onCallback(Map<String,Object> userCompany);
+        }
+    }
+
+
+
+
